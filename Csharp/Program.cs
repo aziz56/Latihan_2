7b104766505f58b1b9df09a60d81e89a7d0aa9c4
BSI80217 <BSI80217@BSINB23L017>
2024-02-21 09:05:10 +0700

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,95 @@
+﻿//class cat
+//{
+//    string? name;
+//    string? color;
+//    int age;
+
+//    public cat(string name, string color, int age)
+//    {
+//        this.name = name;
+//        this.color = color;
+//        this.age = age;
+//    }
+
+//     static void Main(string[] args)
+//    {
+//        cat cat1 = new cat("Tom", "Black", 3);
+//        cat cat2 = new cat("Jerry", "White", 2);
+//        cat cat3 = new cat("Spike", "Brown", 5);
+//        cat cat4 = new cat("Tuffy", "Grey", 1);
+//        Console.WriteLine("Cat1: " + cat1.name + " " + cat1.color + " " + cat1.age);
+//        Console.WriteLine("Cat2: " + cat2.name + " " + cat2.color + " " + cat2.age);
+//        Console.WriteLine("Cat3: " + cat3.name + " " + cat3.color + " " + cat3.age);    
+//        Console.WriteLine("Cat4: " + cat4.name + " " + cat4.color + " " + cat4.age);
+//    }
+//}
+class Time
+{
+    private int hour;
+    private int minute;
+    private int second;
+    private int Date;
+    private int Month;
+    private int Year;
+    private int Day;
+    public int Hour
+    {
+        get { return hour; }
+        set { hour = value; }
+    }
+    public int Minute
+    {
+        get { return minute; }
+        set { minute = value; }
+    }   
+    public int Second
+    {
+        get { return second; }
+        set { second = value; }
+    }
+    public int date
+    {
+        get { return Date; }
+        set { Date = value; }
+    }
+    public int month
+    {
+        get { return Month; }
+        set { Month = value; }
+    }
+    public int year
+    {
+        get { return Year; }
+        set { Year = value; }
+    }
+    public int day
+    {
+        get { return Day; }
+        set { Day = value; }
+    }
+
+
+
+
+
+    public Time()
+    {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
+    public Time(int h, int m, int s)
+    {
+        hour = h;
+        minute = m;
+        second = s;
+    }
+    public void Display()
+    {
+        Console.WriteLine("Time: " + hour + ":" + minute + ":" + second);
+    }
+
+
+}
+
+
---
+++ VB/Module1.vb
@@ -0,0 +1,86 @@
+﻿
+
+Public Class Time
+        Private _year As Integer
+        Private _month As Integer
+        Private _day As Integer
+        Private _hour As Integer
+        Private _minute As Integer
+    Private _second As Integer
+    Public Property year As Integer
+        Get
+            Return _year
+        End Get
+        Set(value As Integer)
+            _year = value
+        End Set
+    End Property
+    Public Property month As Integer
+        Get
+            Return _month
+        End Get
+        Set(value As Integer)
+            _month = value
+        End Set
+    End Property
+    Public Property day As Integer
+        Get
+            Return _day
+        End Get
+        Set(value As Integer)
+            _day = value
+        End Set
+    End Property
+    Public Property hour As Integer
+        Get
+            Return _hour
+        End Get
+        Set(value As Integer)
+            _hour = value
+        End Set
+    End Property
+    Public Property minute As Integer
+        Get
+            Return _minute
+        End Get
+        Set(value As Integer)
+            _minute = value
+        End Set
+    End Property
+    Public Property second As Integer
+        Get
+            Return _second
+        End Get
+        Set(value As Integer)
+            _second = value
+        End Set
+    End Property
+    Public Function ToDate() As Date
+        Return New Date(_year, _month, _day, _hour, _minute, _second)
+    End Function
+
+    Public Sub New()
+            _year = 0
+            _month = 0
+            _day = 0
+            _hour = 0
+            _minute = 0
+            _second = 0
+        End Sub
+        Public Sub New(year As Integer, month As Integer, day As Integer, hour As Integer, minute As Integer, second As Integer)
+            _year = year
+            _month = month
+            _day = day
+            _hour = hour
+            _minute = minute
+            _second = second
+        End Sub
+        Public Sub New(dt As Date)
+            _year = dt.Year
+            _month = dt.Month
+            _day = dt.Day
+            _hour = dt.Hour
+            _minute = dt.Minute
+            _second = dt.Second
+        End Sub
+    End Class
